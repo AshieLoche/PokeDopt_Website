81b717b605548c68e500cb4698a92e1e5809d18f
Ashie Loche <134357122+AshieLoche@users.noreply.github.com>
2024-07-19 20:34:05 +0800

    Unit Test

--- tests/Unit/SignInProcessTest.php
+++ tests/Unit/SignInProcessTest.php
@@ -1,77 +1,65 @@
 <?php
 
-    use PHPUnit\Framework\TestCase;
+use PHPUnit\Framework\TestCase;
 
-    class SignInProcessTest extends TestCase {
-        
-        public function testSignInSuccess()
-        {
-            // Simulate form submission
-            $_POST['signIn'] = true;
-            $_POST['email'] = 'ashie.loche@pokedopt.com';
-            $_POST['password'] = 'ThisIsMyPokedoptYIPPIE!!!<3';
-            $_POST['remember'] = false;
-    
-            $host = 'localhost';
-            $user = 'root';
-            $password = '';
-            $db_name = 'pokedopt';
-        
-            // Connect to Database
-            $conn = mysqli_connect($host, $user , $password, $db_name);
-        
-            // Check Connection
-            if (!$conn) {
-                die('Connection failed: ' . $conn->connect_error);
-            }
-
-            $errorMessage = '';
-            $errors = array(
-                'email' => '',
-                'password' => '',
-            );
-        
-            if (isset($_POST['signIn'])) {
-        
-                if (isset($_POST['email']) && isset($_POST['password'])) {
-                    $email = mysqli_real_escape_string($conn, $_POST['email']);
-                    $password = mysqli_real_escape_string($conn, $_POST['password']);
-                }
-                
-                // Select Pokemon Data
-                $query = "SELECT * FROM account WHERE email = '$email'";
-        
-                // SQL Check
-                if (!mysqli_query($conn, $query)) {
-                    die('Query error: ' . mysqli_error($conn));
-                } else {
-                    // Make Query & Get Result
-                    $result = mysqli_query($conn, $query);
-        
-                    // Fetch the Resulting Rows as an Array
-                    $user = mysqli_fetch_all($result, MYSQLI_ASSOC);
-        
-                    // Free results From Memory
-                    mysqli_free_result($result);
-                }
-        
-                // Close Connection
-                mysqli_close($conn);
-        
-                if (empty($user[0]['username'])) {
-                    $errors['username'] = 'Incorrect username\n';
-                }
-        
-                if (!password_verify($password, $user[0]['password'])) {
-                    $errors['password'] = 'Incorrect password\n';
-                }
-                
-                $this->assertEquals(2, $user['0']['id']);
-        
-            }
-            
+class SignInProcessTest extends TestCase
+{
+    protected function setUp(): void
+    {
+        // Start the session
+        if (session_status() == PHP_SESSION_NONE) {
+            session_start();
         }
-
+        
+        // Clear session
+        $_SESSION = [];
     }
 
+    public function testSignInSuccess()
+    {
+        // Simulate form submission
+        $_POST['signIn'] = true;
+        $_POST['email'] = 'ashie.loche@pokedopt.com';
+        $_POST['password'] = 'ThisIsMyPokedoptYIPPIE!!!<3';
+        $_POST['remember'] = false;
+
+        $host = 'localhost';
+        $user = 'root';
+        $password = '';
+        $db_name = 'pokedopt';
+
+        // Connect to Database
+        $conn = mysqli_connect($host, $user, $password, $db_name);
+
+        // Check Connection
+        if (!$conn) {
+            die('Connection failed: ' . $conn->connect_error);
+        }
+
+        $email = mysqli_real_escape_string($conn, $_POST['email']);
+
+        // Select user data
+        $query = "SELECT * FROM account WHERE email = '$email'";
+        $result = mysqli_query($conn, $query);
+        $user = mysqli_fetch_assoc($result);
+        mysqli_free_result($result);
+        mysqli_close($conn);
+
+        if ($user) {
+            if (password_verify($_POST['password'], $user['password'])) {
+                $_SESSION['userID'] = $user['id'];
+            }
+        }
+
+        // Assert session has been set correctly
+        $this->assertEquals($user['id'], $_SESSION['userID']);
+    }
+
+    protected function tearDown(): void
+    {
+        // Clear session
+        $_SESSION = [];
+        session_destroy();
+    }
+}
 ?>
